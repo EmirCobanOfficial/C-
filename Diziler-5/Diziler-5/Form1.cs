8dfcafadf231ff1a5c30456f0389965553c817b3
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-03-21 12:00:47 +0300

    Add files via upload

---
+++ Diziler-5/Diziler-5/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayılar = { 1,2,3,4,5,6,7,8,9,4,6,8,9,4,3 };
+            int toplam = 0;
+            foreach(int sayı in sayılar)
+            {
+                toplam = toplam + sayı;
+                label1.Text = toplam.ToString();
+            }
+        }
+    }
+}
---
+++ Diziler-6/Diziler-6/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayılar = { 11, 12, 25, 36, 48, 66, 154, -25,-27,-39,-41 };
+            foreach (int sayı in sayılar)
+            {
+                if (sayı %3 ==0 && sayı < 0)
+                {
+                    listBox1.Items.Add(sayı);
+                }
+
+            }
+
+        }
+    }
+}
